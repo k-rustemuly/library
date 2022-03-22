221a3f5b1eb6c9259114e3f7bde070900aa51da6
Kuanysh <developerandroidkz@gmail.com>
2022-03-22 17:06:25 +0500

    0254

--- src/Domain/Sign/Service/SignIn.php
+++ src/Domain/Sign/Service/SignIn.php
@@ -83,7 +83,7 @@ final class SignIn {
         $email = isset($data['email']) ? $data['email'] : "";
         $password = isset($data['password']) ? $data['password'] : "";
         $adminInfo = $this->readRepository->findByEmail($email);
-        return password_verify($password, $adminInfo["password"]);
+        return array("a" => password_verify($password, $adminInfo["password"]));
         if(empty($adminInfo) || !password_verify($password, $adminInfo["password"])) {
             throw new DomainException("Email or password is incorrect");
         }
