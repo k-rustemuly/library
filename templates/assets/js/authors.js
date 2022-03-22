848f860b36313f4eff8b365080fbc8dc4d7c04ef
Kuanysh <developerandroidkz@gmail.com>
2022-03-23 01:07:55 +0500

    search

--- templates/assets/js/authors.js
+++ templates/assets/js/authors.js
@@ -1,2 +1,14 @@
 "use strict";
-$('#authors').DataTable();
+const t = document.getElementById("authors");
+const e = $(t).DataTable({
+    info: !1,
+    order: [],
+    columnDefs: [{
+        targets: 4,
+        orderable: !1
+    }]
+});
+var r = document.getElementById("kt_filter_search");
+r && r.addEventListener("keyup", (function (t) {
+    e.search(t.target.value).draw()
+}))
