43bd9110616b3ed9594f68f47372a22ba3e371b8
Kuanysh <developerandroidkz@gmail.com>
2022-03-23 16:18:42 +0500

    sdf

--- templates/assets/js/book.js
+++ templates/assets/js/book.js
@@ -1,7 +1,16 @@
 "use strict";
+
+
+$('#isbn').maxlength({
+    warningClass: "badge badge-warning",
+    limitReachedClass: "badge badge-success"
+});
+
+
 const t = document.getElementById("table");
 const e = $(t).DataTable();
 var r = document.getElementById("kt_filter_search");
 r && r.addEventListener("keyup", (function (t) {
     e.search(t.target.value).draw()
 }))
+
