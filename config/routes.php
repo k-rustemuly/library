09fef5e11b86e49c5a4b89e259bdd03788ab2526
Kuanysh <developerandroidkz@gmail.com>
2022-04-07 12:36:47 +0500

    fix bug

--- config/routes.php
+++ config/routes.php
@@ -34,7 +34,7 @@ return function (App $app) {
 
                 $app->group('/terminal', function (RouteCollectorProxy $app) { 
 
-                    $app->get('', \App\Action\Terminal\ReadACtion::class);
+                    $app->get('', \App\Action\Terminal\ReadAction::class);
 
                 });
 
