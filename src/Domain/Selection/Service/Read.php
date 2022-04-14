8f73455d6a06f9a5f81097b357b2cccb3e9849a9
Kuanysh <developerandroidkz@gmail.com>
2022-04-14 11:42:35 +0500

    pokhr

--- src/Domain/Selection/Service/Read.php
+++ src/Domain/Selection/Service/Read.php
@@ -99,19 +99,19 @@ final class Read extends Admin{
             $list[$i]["tags"] = $this->parseTags($tags);
         }
         if(count(array_keys($this->tags)) > 0) {
-            // $tag_list = $this->tagFinderRepository->getByIds(array_keys($this->tags));
-            // $tags = array();
-            // foreach ($tag_list as $tag) {
-            //     $tags[$tag["id"]] = $tag["name"];
-            // }
-            // foreach ($list as $i => $item){
-            //     $string = "";
-            //     $ids = $item["tags"];
-            //     foreach ($ids as $id) {
-            //         $string.=$tags[$id].", ";
-            //     }
-            //     $list[$i]["tags"] = substr($string, 0, -2);
-            // }    
+            $tag_list = $this->tagFinderRepository->getByIds(array_keys($this->tags));
+            $tags = array();
+            foreach ($tag_list as $tag) {
+                $tags[$tag["id"]] = $tag["name"];
+            }
+            foreach ($list as $i => $item){
+                $string = "";
+                $ids = $item["tags"];
+                foreach ($ids as $id) {
+                    $string.=$tags[$id].", ";
+                }
+                $list[$i]["tags"] = substr($string, 0, -2);
+            }    
         }
         return $list;
     }
