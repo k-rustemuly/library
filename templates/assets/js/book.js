d8dd337faba58bd46a38da87251cbcfb763986dc
Kuanysh <developerandroidkz@gmail.com>
2022-04-14 15:46:42 +0500

    sdf

--- templates/assets/js/book.js
+++ templates/assets/js/book.js
@@ -67,8 +67,49 @@ $(".book-edit").on('click', function (e) {
     $("i", this).removeClass("la-pencil-alt");
     $("i", this).addClass('la-spinner');
     var i = $("i", this);
+    $.get(languageUrl, function (data) {
+        var languages = data.data;
+        $('#editLanguageCode').empty();
+        for (var i = 0; i < languages.length; i++) {
+            $('#editLanguageCode').append(new Option(languages[i].name, languages[i].code));
+        }
+        $('#editLanguageCode').select2();
+    });
+    $.get(publisherUrl, function (data) {
+        var d = data.data;
+        $('#editPublisher').empty();
+        for (var i = 0; i < d.length; i++) {
+            $('#editPublisher').append(new Option(d[i].name, d[i].id));
+        }
+        $('#editPublisher').select2();
+    });
+    $.get(seriesUrl, function (data) {
+        var d = data.data;
+        $('#editSeries').empty();
+        for (var i = 0; i < d.length; i++) {
+            $('#editSeries').append(new Option(d[i].name, d[i].id));
+        }
+        $('#editSeries').select2();
+    });
+    $.get(authorList, function (data) {
+        var d = data.data;
+        $('#editAuthors').empty();
+        for (var i = 0; i < d.length; i++) {
+            $('#editAuthors').append(new Option(d[i].name, d[i].id));
+        }
+        $('#editAuthors').select2();
+    });
     $.get(bookSearchUrl, {id: id}, function (data) {
         var d = data.data;
+        $("#editIsbn").val(d.isbn);
+        $("#editName").val(d.name);
+        $("#editImage").css("background-image", "url(" + d.image + ")");
+        $("#editPageCount").val(d.page_count);
+        $('#editPubYear option[value="'+d.published_year+'"]').prop('selected', true);
+        $('#editLanguageCode option[value="'+d.language_code+'"]').prop('selected', true);
+        $('#editPublisher option[value="'+d.publisher_id+'"]').prop('selected', true);
+        $('#editSeries option[value="'+d.publisher_id+'"]').prop('selected', true);
+        $("#editDescription").val(d.description);
         $("#book_edit").modal('show');
         i.removeClass("la-spinner");
         i.addClass('la-pencil-alt');
